d9907189a48cbcfecaa241e99f1713dccd1d3e98
andreluas <andreluas.k@gmail.com>
2023-11-06 21:10:05 -0300

    prisma service configs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
